3a086b75092f9fa730fe3ae323c43f37eb7ec389
ich777 <ich777@noreply.example.org>
2019-09-06 09:17:25 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -2,3 +2,10 @@
 
 echo "---Sleep zZz---"
 sleep infinity
+
+INSTALL_V="$(echo ${DL_URL} | cut -d '/' -f6)"
+
+
+cd ${DATA_DIR}
+echo "---Downloading install script version: $INSTALL_V---"
+wget -qi install.py ${DL_URL}
