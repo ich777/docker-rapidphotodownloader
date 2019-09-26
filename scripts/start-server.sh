0b4c299341e0a6c3106c22bc4e913c00c0fbfefb
ich777 <ich777@noreply.example.org>
2019-09-26 20:33:26 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+echo "---Starting Xvfb server---"
+screen -S Xvfb -L -Logfile ${DATA_DIR}/XvfbLog.0 -d -m /opt/scripts/start-Xvfb.sh
+sleep 5
+echo "---Starting x11vnc server---"
+screen -S x11vnc -L -Logfile ${DATA_DIR}/x11vncLog.0 -d -m /opt/scripts/start-x11.sh
+sleep 5
+
+echo "---Starting noVNC server---"
+websockify -D --web=/usr/share/novnc/ --cert=/etc/ssl/novnc.pem 8080 localhost:5900
+sleep 5
+
 echo "---Sleep zZz---"
 sleep infinity
 
@@ -20,15 +31,6 @@ find /tmp -name ".X99*" -exec rm -f {} \;
 chmod -R 770 ${DATA_DIR}
 
 
-echo "---Starting Xvfb server---"
-screen -S Xvfb -L -Logfile ${DATA_DIR}/XvfbLog.0 -d -m /opt/scripts/start-Xvfb.sh
-sleep 5
-echo "---Starting x11vnc server---"
-screen -S x11vnc -L -Logfile ${DATA_DIR}/x11vncLog.0 -d -m /opt/scripts/start-x11.sh
-sleep 5
 
-echo "---Starting noVNC server---"
-websockify -D --web=/usr/share/novnc/ --cert=/etc/ssl/novnc.pem 8080 localhost:5900
-sleep 5
 
 echo "---Starting Rapid Photo Downloader---"
