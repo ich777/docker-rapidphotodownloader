c1e130e11f1bfc0271008882cc8b12f0e279694a
ich777 <ich777@noreply.example.org>
2019-09-29 16:39:44 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -10,11 +10,21 @@ if [ "${FORCE_UPDATE}" == "true" ]; then
 	echo "---------This can takes some time-----------"
 	echo "--------depending on your hardware----------"
 	echo "--------------------------------------------"
+    sleep 10
     cd ${DATA_DIR}
 	if [ -f "${DATA_DIR}/install.py" ]; then
     	rm ${DATA_DIR}/install.py
     fi
     wget "${DL_URL}"
+	if [ ! -f "${DATA_DIR}/install.py" ]; then
+		echo "---------------------------------------------"
+		echo "---Something went wrong, couldn't download---"
+		echo "---'install.py' please check the download----"
+		echo "---link or place the file manually in the----"
+		echo "----------root of your serverfolder----------"
+		echo "---------------------------------------------"
+		sleep infinity
+	fi
 	python3 -m venv ${DATA_DIR}/rpd
 	source ${DATA_DIR}/rpd/bin/activate
 	python3 install.py --virtual-env
@@ -26,9 +36,19 @@ else
     	echo "-------This can takes some time-------"
     	echo "------depending on your hardware------"
     	echo "--------------------------------------"
+        sleep 10
 		cd ${DATA_DIR}
 		if [ ! -f "${DATA_DIR}/install.py" ]; then
 			wget "${DL_URL}"
+            if [ ! -f "${DATA_DIR}/install.py" ]; then
+            	echo "---------------------------------------------"
+            	echo "---Something went wrong, couldn't download---"
+            	echo "---'install.py' please check the download----"
+            	echo "---link or place the file manually in the----"
+            	echo "----------root of your serverfolder----------"
+            	echo "---------------------------------------------"
+            	sleep infinity
+            fi
 		fi
 		python3 -m venv ${DATA_DIR}/rpd
 		source ${DATA_DIR}/rpd/bin/activate
