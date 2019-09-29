0139500aa21edc76afbb891bbec1c653b92b4bd0
ich777 <ich777@noreply.example.org>
2019-09-29 09:43:26 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "---Preparing Server---"
+echo "---Checking for old logfiles---"
+find $DATA_DIR -name "XvfbLog.*" -exec rm -f {} \;
+find $DATA_DIR -name "x11vncLog.*" -exec rm -f {} \;
+echo "---Checking for old display lock files---"
+find /tmp -name ".X99*" -exec rm -f {} \;
+chmod -R 770 ${DATA_DIR}
+
 echo "---Starting Xvfb server---"
 screen -S Xvfb -L -Logfile ${DATA_DIR}/XvfbLog.0 -d -m /opt/scripts/start-Xvfb.sh
 sleep 5
@@ -11,26 +19,9 @@ echo "---Starting noVNC server---"
 websockify -D --web=/usr/share/novnc/ --cert=/etc/ssl/novnc.pem 8080 localhost:5900
 sleep 5
 
-echo "---Sleep zZz---"
-sleep infinity
-
-INSTALL_V="$(echo ${DL_URL} | cut -d '/' -f6)"
-
-
-cd ${DATA_DIR}
-echo "---Downloading install script version: $INSTALL_V---"
-wget -qi install.py ${DL_URL}
-
-
-echo "---Preparing Server---"
-echo "---Checking for old logfiles---"
-find $DATA_DIR -name "XvfbLog.*" -exec rm -f {} \;
-find $DATA_DIR -name "x11vncLog.*" -exec rm -f {} \;
-echo "---Checking for old display lock files---"
-find /tmp -name ".X99*" -exec rm -f {} \;
-chmod -R 770 ${DATA_DIR}
-
-
-
+export DISPLAY=:99
 
 echo "---Starting Rapid Photo Downloader---"
+
+echo "---Sleep zZz---"
+sleep infinity
