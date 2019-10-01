7b9091277be3b02c6e168fce62289d1b18a900b6
ich777 <ich777@noreply.example.org>
2019-10-01 11:11:31 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -93,18 +93,9 @@ if [ "${CUSTOM_RES_W}" -le 999 ]; then
 	echo "---Width to low must be a minimal of 1000 pixels, correcting to 1000...---"
     CUSTOM_RES_W=1000
 fi
-if [ "${CUSTOM_RES_H}" -le 880 ]; then
+if [ "${CUSTOM_RES_H}" -le 879 ]; then
 	echo "---Height to low must be a minimal of 880 pixels, correcting to 880...---"
     CUSTOM_RES_H=880
-fi
-
-WINDOWRES=$(grep -e 'windowSize=@Size(.... ' ${DATA_DIR}/.config/Rapid\ Photo\ Downloader/Rapid\ Photo\ Downloader.conf)
-
-if [ "$WINDOWRES" != "windowSize=@Size(${CUSTOM_RES_W} ${CUSTOM_RES_H})" ]; then
-	echo "---Window resoltuion changed to ${CUSTOM_RES_W}x${CUSTOM_RES_H}, writing to config file---"
-	sed -i '/$WINDOWRES/c\windowSize=@Size(${CUSTOM_RES_W} ${CUSTOM_RES_H})' "${DATA_DIR}/.config/Rapid Photo Downloader/Rapid Photo Downloader.conf"
-else
-	echo "---Window resolution: ${CUSTOM_RES_W}x${CUSTOM_RES_H}---"
 fi
 
 if [ ! -f "${DATA_DIR}/.config/Rapid Photo Downloader/Rapid Photo Downloader.conf" ]; then
@@ -128,6 +119,15 @@ this_computer_source=true
 
 [Rename]
 photo_download_folder=/media" >> "${DATA_DIR}/.config/Rapid Photo Downloader/Rapid Photo Downloader.conf"
+fi
+
+WINDOWRES=$(grep -e 'windowSize=@Size(.... ' ${DATA_DIR}/.config/Rapid\ Photo\ Downloader/Rapid\ Photo\ Downloader.conf)
+
+if [ "$WINDOWRES" != "windowSize=@Size(${CUSTOM_RES_W} ${CUSTOM_RES_H})" ]; then
+	echo "---Window resoltuion changed to ${CUSTOM_RES_W}x${CUSTOM_RES_H}, writing to config file---"
+	sed -i '/$WINDOWRES/c\windowSize=@Size(${CUSTOM_RES_W} ${CUSTOM_RES_H})' "${DATA_DIR}/.config/Rapid Photo Downloader/Rapid Photo Downloader.conf"
+else
+	echo "---Window resolution: ${CUSTOM_RES_W}x${CUSTOM_RES_H}---"
 fi
 
 echo "---Preparing Server---"
